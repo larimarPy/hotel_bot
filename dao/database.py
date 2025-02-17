db70e765e51886b6034a368dc05125356cc90d3e
larimarPy <mumatovsunnat2025@gmail.com>
2025-02-17 18:54:22 +0200

    commit

---
+++ dao/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+
+from dao.models import Base
+
+DATABASE_URL = "sqlite+aiosqlite:///data/my_db.db"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+sessionmaker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def create_db():
+    async with engine.begin() as session:
+        await session.run_sync(Base.metadata.create_all)
+        
+async def drop_db():
+    async with engine.begin() as session:
+        await session.run_sync(Base.metadata.drop_all)
--- main.py
+++ main.py
@@ -7,16 +7,36 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
+from middleware.db import DataBaseSession
+
+from dao.database import create_db, drop_db, sessionmaker
+
 from handlers.user_private import upr
 from common.commands import cmds
 
 bot = Bot(token=os.getenv("TOKEN"))
-dp = Dispatcher(storage=MemoryStorage)
+dp = Dispatcher()
 
 dp.include_router(upr)
 
 
+async def on_startup(bot):
+
+    run_param = False
+    if run_param:
+        await drop_db()
+
+    await create_db()
+
+async def on_shutdown(bot):
+    print('бот лег')
+
 async def main():
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
+    dp.update.middleware(DataBaseSession(session_pool=sessionmaker))
+
     await bot.delete_webhook(drop_pending_updates=True)
     await bot.set_my_description(description="Привет, меня зовут Грэй и я готов помочь тебе!(Любой текст здесь)")
     await bot.set_my_commands(commands=cmds, scope=types.BotCommandScopeAllPrivateChats())
